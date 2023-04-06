961da996280730e27b79a8ad3704646162cc122f
Bartosz Bartosik <babosz189@gmail.com>
2023-04-06 17:35:09 +0200

    Added dft() function

--- main.py
+++ main.py
@@ -1,49 +1,78 @@
-from typing import Callable
-
 import matplotlib.pyplot as plt
 import numpy as np
 
-def integrate(function: Callable, domain: np.ndarray, step: float):
-    x = domain
-    y = np.array([function(x) for x in x])
+
+def integrate(x: np.ndarray, y, retarray=True):
+    dx =  x[1] - x[0]
 
     sum = 0
-    y_int = []
-    for y in y:
-        sum += step*y
-        y_int.append(sum)
+    if retarray:
+        y_int = []
+        for y_i in y:
+            sum += dx * y_i
+            y_int.append(sum)
 
-    return np.array(y_int)
+        return np.array(y_int)
+    else:
+        for y_i in y:
+            sum += dx * y_i
+        return sum
 
-def ft(function: Callable):
-    pass
+
+def dft(x: np.ndarray, y: np.ndarray):
+
+    n = len(x)
+    df = (x[-1]-x[0])/n
+
+    f = np.linspace(0, 1/(2*df), n)
+
+    F = []
+    for f_i in f:
+        alpha = integrate(x, y*np.cos(f_i*x), retarray=False)
+        beta = integrate(x, y*np.sin(f_i*x), retarray=False)
+
+        F.append(np.sqrt(alpha**2 + beta**2))
+
+    return f, F
 
 def function(x):
-    return -x+2
+    return np.sin(2*x) + 2*np.cos(5*x) + np.cos(20*x + 1) + np.sin(110*x + 2) + 1.5*np.cos(50*x)
 
 def main():
+    # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
+    # # # # # # # # # # # # # # # # # # # # # # # # #   S E T   U P   # # # # # # # # # # # # # # # # # # # # # # # # #
+    # Number of samples per unit
+    f_s = 500
 
-    x, step = np.linspace(-10, 10, 1000, retstep=True)
+    # x's increment
+    dx = 1/f_s
+
+    # Generate a domain
+    x = np.arange(0, 1*np.pi, dx)
+
+    # Generate a function
     y = np.array([function(x) for x in x])
-    y_int = integrate(function, x, step)
 
-    # print('integral: {}'.format(integrate(function, x, step)))
+    # Fourier transform
+    f, F = dft(x, y)
 
+    # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
+    # # # # # # # # # # # # # # # # # # # # # # # # # #   P L O T   # # # # # # # # # # # # # # # # # # # # # # # # # #
     fig, ax = plt.subplots(2, 1)
 
-    ax[0].plot(x, y)
-    ax[0].axis('equal')
+    # Function
+    ax[0].scatter(x, y, s=0.2)
+    # ax[0].axis('equal')
     ax[0].set_xlabel('x')
     ax[0].set_ylabel('f(x)')
     ax[0].grid()
 
-    ax[1].plot(x, y_int)
-    ax[1].axis('equal')
-    ax[1].set_xlabel('x')
-    ax[1].set_ylabel('F(x)')
+    # Fourier transform
+    ax[1].plot(f, F)
+    ax[1].set_xlabel('f')
+    ax[1].set_ylabel('F(f)')
     ax[1].grid()
 
-    # plt.grid()
     plt.show()
 
 
