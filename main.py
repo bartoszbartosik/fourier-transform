87ce4316c45b75d03fd4eab0bd143fd0d1a1809a
Bartosz Bartosik <babosz189@gmail.com>
2023-11-04 22:41:47 +0100

    Added data postprocessing section; created new plots

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import matplotlib.pyplot as plt
 import numpy as np
+from numpy import sin, cos, pi
 
 # Integrate an y(x) function
 def integrate(x: np.ndarray, y, return_array=False):
@@ -19,13 +20,12 @@ def integrate(x: np.ndarray, y, return_array=False):
         return da
 
 # Perform a Discrete Fourier Transform on a y(x) function
-# TODO: add phase shift
-def dtft(x: np.ndarray, y: np.ndarray):
+def dtft(x: np.ndarray, y: np.ndarray) -> dict:
     # Number of samples
     n = len(x)
 
     # Frequency increment (sampling frequency reciprocal)
-    df = (x[-1]-x[0])/n
+    df = (x[1]-x[0])
 
     # Symmetry axis
     f_sym = 1/(2*df)*x[-1]
@@ -36,57 +36,100 @@ def dtft(x: np.ndarray, y: np.ndarray):
     # Compute Fourier Transform
     results = {
         'f': f,
-        're': [],
-        'im': []
+        're': np.array([]),
+        'im': np.array([])
     }
     for f_i in f:
-        re = integrate(x, y*np.cos(f_i*x))
-        im = integrate(x, y*np.sin(f_i*x))
+        re = integrate(x, y*cos(f_i*x))
+        im = integrate(x, y*sin(f_i*x))
 
-        results['re'].append(re)
-        results['im'].append(im)
+        results['re'] = np.append(results['re'], re)
+        results['im'] = np.append(results['im'], im)
 
     return results
 
-# Define a function for which Fourier Transform will be performed
+# Define a function to be analysed
 def function(x):
-    return np.sin(2*x) + 2*np.cos(5*x) + np.cos(20*x + 1) + np.sin(110*x + 2) + 1.5*np.cos(50*x)
+    return sin(2*x) + 2*cos(5*x) + cos(20*x + 1.71) + sin(110*x -1.71) + 1.5*cos(50*x)
 
 # Main function
 def main():
     # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
     # # # # # # # # # # # # # # # # # # # # # # # # #   S E T   U P   # # # # # # # # # # # # # # # # # # # # # # # # #
-    # Sampling frequency
-    f_s = 100
+    # Sampling frequency (number of samples per unit)
+    f_s = 200
 
-    # x's increment
+    # Function interval
+    x_end = 2
+
+    # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
+
+    # Function domain increment
     dx = 1/f_s
 
     # Generate a domain
-    x = np.arange(0, 2*np.pi, dx)
+    x = np.arange(0, x_end, dx)
 
     # Generate a function
     y = np.array([function(x) for x in x])
 
-    # Fourier transform
+    # Compute Fourier transform
     dtft_dict = dtft(x, y)
+
+
+    # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
+    # # # # # # # # # # # # # # # # # # #   D A T A   P O S T P R O C E S S I N G   # # # # # # # # # # # # # # # # # #
+    # Extract frequency, real and imaginary part, subsequently
+    f, re, im = dtft_dict.values()
+
+    # Compute each frequency magnitude
+    mags = np.sqrt(re**2 + im**2)
+
+    # Compute each frequency phase shift
+    phi = np.arctan(im/re)
+
 
     # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
     # # # # # # # # # # # # # # # # # # # # # # # # # #   P L O T   # # # # # # # # # # # # # # # # # # # # # # # # # #
-    fig, ax = plt.subplots(2, 1)
+    # Analysed function
+    plt.subplot(311)
+    plt.scatter(x, y, s=0.2)
+    plt.title('Analysed function')
+    plt.xlabel('x')
+    plt.ylabel('f(x)')
+    plt.grid()
 
-    # Function
-    ax[0].scatter(x, y, s=0.2)
-    # ax[0].axis('equal')
-    ax[0].set_xlabel('x')
-    ax[0].set_ylabel('f(x)')
-    ax[0].grid()
+    # Magnitude
+    plt.subplot(323)
+    plt.plot(f, mags)
+    plt.title('Magnitude')
+    plt.xlabel('f')
+    plt.ylabel('M(f)')
+    plt.grid()
 
-    # Fourier transform
-    ax[1].plot(f, ft)
-    ax[1].set_xlabel('f')
-    ax[1].set_ylabel('F(f)')
-    ax[1].grid()
+    # Phase shift
+    plt.subplot(324)
+    plt.plot(f, phi)
+    plt.title('Phase shift')
+    plt.xlabel('f')
+    plt.ylabel('phi(f)')
+    plt.grid()
+
+    # Real part
+    plt.subplot(325)
+    plt.plot(f, re)
+    plt.title('Real part')
+    plt.xlabel('f')
+    plt.ylabel('re(f)')
+    plt.grid()
+
+    # Imaginary part
+    plt.subplot(326)
+    plt.plot(f, im)
+    plt.title('Imaginary part')
+    plt.xlabel('f')
+    plt.ylabel('im(f)')
+    plt.grid()
 
     plt.show()
 
